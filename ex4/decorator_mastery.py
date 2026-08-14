02200d3e8b1edc6943573530614ca36f06905d1e
WeyWan753 <syahmiazman753@gmail.com>
2026-08-14 23:41:09 +0800

    1

--- ex4/decorator_mastery.py
+++ ex4/decorator_mastery.py
@@ -23,10 +23,10 @@ def power_validator(min_power: int) -> Callable:
         def wrapper(*args, **kwargs):
             power = kwargs.get('power')
             if power is None:
-                return f"power keyword argument is not passed"
+                return "power keyword argument is not passed"
             if power < min_power:
-                return f"insufficient power for this spell"
-            return(func(*args, **kwargs))
+                return "insufficient power for this spell"
+            return (func(*args, **kwargs))
         return wrapper
     return decorator
 
@@ -43,12 +43,14 @@ def retry_spell(max_attempts: int) -> Callable:
                 try:
                     x = func(*args, **kwargs)
                     print(f"congrat, your function passed at {i + 1} attempts")
-                    return(x)
-                except Exception as e:
+                    return (x)
+                except Exception:
                     if i != max_attempts - 1:
-                        print(f"Spell failed, retrying... (attempt {i + 1}/{max_attempts})")
+                        print(f"Spell failed, retrying... "
+                              f"(attempt {i + 1}/{max_attempts})")
                     else:
-                        print(f"Spell casting failed after {max_attempts} attempts")
+                        print(f"Spell casting failed "
+                              f"after {max_attempts} attempts")
                         print("Waaaaaaagh spelled !")
         return wrapper
     return decorator
@@ -57,14 +59,16 @@ def retry_spell(max_attempts: int) -> Callable:
 class MageGuild:
     @staticmethod
     def validate_mage_name(name: str) -> bool:
-        if not isinstance(name ,str):
+        if not isinstance(name, str):
             return False
-        return len(name.replace(" ","")) >= 3 and all(c.isalpha() or c.isspace() for c in name)
+        return (
+            len(name.replace(" ", "")) >= 3 and
+            all(c.isalpha() or c.isspace() for c in name)
+        )
 
-    @power_validator(50)
+    @power_validator(10)
     def cast_spell(self, spell_name: str, power: int) -> str:
-        """MEGANIGGA"""
-        return f"casted {spell_name} at {power} power"
+        return f"Successfully cast {spell_name} with {power} power"
 
 
 def main() -> None:
@@ -78,8 +82,9 @@ def main() -> None:
     print()
 
     print("Testing retrying spell...")
-    
+
     x = 0
+
     @retry_spell(3)
     def test_retry_1():
         nonlocal x
@@ -87,11 +92,12 @@ def main() -> None:
         if x < 4:
             raise Exception()
         return "Success"
-    
+
     print(f"result = {test_retry_1()}")
     print()
 
     x = 0
+
     @retry_spell(8)
     def test_retry_2():
         nonlocal x
@@ -101,6 +107,14 @@ def main() -> None:
         return "Success"
 
     print(f"result = {test_retry_2()}")
+    print()
+
+    print("Testing MageGuild...")
+    mg = MageGuild()
+    print(mg.validate_mage_name("ab c"))
+    print(mg.validate_mage_name("b    c"))
+    print(mg.cast_spell("fireball", power=15))
+    print(mg.cast_spell("fireball", power=5))
 
 
 if __name__ == "__main__":
