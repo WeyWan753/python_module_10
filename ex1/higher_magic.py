191f92e6248d72fcbd3a60bee4497bfd139e1f05
WeyWan753 <syahmiazman753@gmail.com>
2026-08-14 18:34:28 +0800

    1

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -14,32 +14,47 @@ def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
 
 
 def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
-    return lambda target, power: base_spell(target, power * multiplier)
+    return (
+        lambda target, power: f"orignal: {power}, amplified: " +
+        base_spell(target, power * multiplier)
+    )
 
 
 def conditional_caster(condition: Callable, spell: Callable) -> Callable:
     return (
-        lambda target, power: spell(target, power)
+        lambda target, power: "Condition satisfied: " + spell(target, power)
         if condition(target, power)
-        else "Spell fizzled"
+        else "Conditions not met: Spell fizzled"
     )
 
 
 def spell_sequence(spells: list[Callable]) -> Callable:
-    return lambda target, power: [spell(target, power) for spell in spells]
+    return (
+        lambda target, power:
+        ", ".join([f"{i + 1}) {spells[i](target, power)}"
+                  for i in range(len(spells))])
+    )
 
 
 if __name__ == "__main__":
+    print("Testing spell combiner...")
     combined = spell_combiner(heal, fireball)
-    print(combined("Ali", 2))
+    print(", ".join([x for x in combined("Ali", 2)]))
+    print()
 
+    print("Testing power amplifier...")
     amplified = power_amplifier(fireball, 3)
     print(amplified("Ali", 2))
+    print()
 
     def condition(target: str, power: int) -> bool:
         return (True if target == "Ali" and power == 2 else False)
-    conditional = conditional_caster(condition, fireball)
-    print(conditional("Ali", 3))
 
+    print("Testing conditional caster...")
+    conditional = conditional_caster(condition, fireball)
+    print(conditional("Ali", 4))
+    print()
+
+    print("Testing spell sequence...")
     sequence = spell_sequence([heal, fireball])
     print(sequence("Ali", 4))
